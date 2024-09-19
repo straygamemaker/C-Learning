575534afd6e7fa9482d581d00a768124e1f95b75
Sawyer <146210310+straygamemaker@users.noreply.github.com>
2024-09-19 14:14:03 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,3 +5,29 @@ int main()
 
     cout << "Hello World";
 }
+
+
+
+
+
+
+
+History
+
+Q 1 : What is the name of your language? 
+	The language is C++.
+Q2:  When/where was it created, and by whom? Was it written to address a particular problem or need? 
+	It was created by Danish Computer Scientist Bjarne Stroustrup at Bell Telephone Laboratories in Murray Hill New Jersey. It was made to make a more efficient way of coding, but also one that has high level features for optimal organization. It was meant to be an essential upgrade from the regular C coding language. 
+Q3: What types of programming is your language primarily used for (ex: web sites, video games, mobile devices, parsing, etc.)? If your language is multi-purpose, provide some examples of different projects it’s been used for. 
+	C++ is definitely a multi-use language. It is used for game development, which is the reason I chose it. It is very big in the financial world as well, and large scale web services like Google. Many game engines use C++, including the popular engines Godot, HeroEngine, Unity, Unreal, and many more. The backend of Spotify is managed with C++, as well as big games like World of Warcraft and Counter-Strike. 
+Q4: Where will you get information about this language when it's time to start programming in it? At the end of your assignment, provide a list of the names of books, website URLs, or any other resources that relate to your language in particular.
+	I think for websites I will be referencing youtube tutorials, w3schools, and codecademy. I will keep track of every link / book I use. 
+
+Getting Started Complete
+
+Follow up questions: 
+	To install the language, you need a computer of some kind, a laptop or PC. You will need an IDE to actually run and type the code, I am using Visual Studio Code. You will need to install the C/C++ extension in Visual Studio Code. If you are on mac, it is a bit more difficult to run, and I would recommend installing x code. You can install that in the app store, and you will need clang as well, which is a tool for running C. I have seen different recommendations from people online as to the best environment for C++, but generally Visual Studio is the best for its helpful extensions and ease of use. I picked visual studio code because of the recommendations I saw and because I am very familiar with it. You can run programs in the language inside VSC by pressing run at the top right, not debug. Comments in C++ are written: 
+// like this for one line
+
+/* or like this
+for multiple lines */       keep the stars on the inside of the slashes like a sandwich
